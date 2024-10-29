e4f3fca1f1fce827b83d7c7053149798d44d256a
iamsufiyan560 <msufiyanhusen@gmail.com>
2024-10-29 13:54:30 +0530

    add navbar and logout modal

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/base/Navbar";
+import React from "react";
+
+export default async function dashboard() {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
+}
